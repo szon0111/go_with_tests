9cdad6ca3d23334804546ae46e58c1fe9c07ee8b
szon0111 <szon0111@gmail.com>
2020-04-12 11:29:29 +0900

    iteration - add ExampleRepeat

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 3)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", 3)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 4)
+	fmt.Println(repeated)
+	// Output: aaaa
 }
