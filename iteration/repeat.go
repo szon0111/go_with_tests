88447bb8dd91756be888f5cf78d7b8479c608c32
szon0111 <szon0111@gmail.com>
2020-04-12 11:24:54 +0900

    iteration - add repeatCount

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, repeatNum int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatNum; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 3)
 	}
 }
