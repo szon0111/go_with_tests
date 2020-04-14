b82eecdd9526f5e6036d36c42999d3e1f543ce29
szon0111 <szon0111@gmail.com>
2020-04-15 00:38:15 +0900

    points_errors - initial commit

---
+++ pointers_errors/wallet_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+
+	fmt.Printf("address of balance in Test is %v \n", &wallet.balance)
+
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
