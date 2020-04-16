120d02a8b51b4151535e07249eae44545b6ae271
szon0111 <szon0111@gmail.com>
2020-04-16 23:59:27 +0900

    mocking - add for loop

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
+}
+
+func main() {
+	Countdown(os.Stdout)
 }
